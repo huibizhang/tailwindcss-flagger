fb2c4f53f0060b0219e43043a3c75ebdc3d33fb8
搋兔 <naruto6404@hotmail.com>
2021-08-16 09:45:12 +0800

    add navigate link

--- docs/components/Header.vue
+++ docs/components/Header.vue
@@ -1,7 +1,11 @@
 <template>
   <header class="flex flex-col gap-5 items-center mb-20">
     <h1 class="text-5xl font-black font-mono">Flagger</h1>
-    <span class="block text-xl text-gray-400 font-bold font-mono">TailwindCSS Plugin</span>
+    <span class="block text-xl text-gray-400 font-bold font-mono">🚩 TailwindCSS Plugin</span>
+    <div class="max-w-md flex gap-10">
+      <a href="#installation">Installation</a>
+      <a href="#usage">Usage</a>
+    </div>
   </header>
 </template>
 
