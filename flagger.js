c66596d6dfd439e854945db5bbf840042b0bdbac
搋兔 <naruto6404@hotmail.com>
2022-03-01 10:18:11 +0800

    update: use `pair` instead `flag`

--- flagger.js
+++ flagger.js
@@ -2,36 +2,97 @@ const _ = require('lodash')
 const plugin = require('tailwindcss/plugin')
 
 const flagger = plugin(
-  function ({ addUtilities, theme, e, addVariant, config }) {
-    const flagUtilities = _.map(theme('flag'), (value, key) => {
-      return {
-        [`.${e(`flag-${key}`)}`]: {},
-      }
+  ({ addUtilities, theme, addVariant }) => {
+    const pairUtilities = Object.keys(theme('pair', {}))
+    pairUtilities.forEach((key) => {
+      addUtilities({
+        [`.pair-${key}`]: {},
+      })
     })
-    addUtilities(flagUtilities)
 
-    const prevarient = ['checked', 'hover', 'active', 'disabled', 'focus']
-    _.map(theme('flag'), (value, key) => {
-      prevarient.forEach((variant) => {
-        addVariant(`flag-${key}-${variant}`, ({ modifySelectors, separator }) => {
+    let pseudoVariants = [
+      // Positional
+      ['first', ':first-child'],
+      ['last', ':last-child'],
+      ['only', ':only-child'],
+      ['odd', ':nth-child(odd)'],
+      ['even', ':nth-child(even)'],
+      'first-of-type',
+      'last-of-type',
+      'only-of-type',
+
+      // State
+      [
+        'visited',
+        ({ container }) => {
+          let toRemove = ['--tw-text-opacity', '--tw-border-opacity', '--tw-bg-opacity']
+
+          container.walkDecls((decl) => {
+            if (toRemove.includes(decl.prop)) {
+              decl.remove()
+
+              return
+            }
+
+            for (const varName of toRemove) {
+              if (decl.value.includes(`/ var(${varName})`)) {
+                decl.value = decl.value.replace(`/ var(${varName})`, '')
+              }
+            }
+          })
+
+          return ':visited'
+        },
+      ],
+      'target',
+      ['open', '[open]'],
+
+      // Forms
+      'default',
+      'checked',
+      'indeterminate',
+      'placeholder-shown',
+      'autofill',
+      'required',
+      'valid',
+      'invalid',
+      'in-range',
+      'out-of-range',
+      'read-only',
+
+      // Content
+      'empty',
+
+      // Interactive
+      'focus-within',
+      'hover',
+      'focus',
+      'focus-visible',
+      'active',
+      'disabled',
+    ].map((variant) => (Array.isArray(variant) ? variant : [variant, `:${variant}`]))
+
+    for (let [variantName, state] of pseudoVariants) {
+      Object.keys(theme('pair', {})).forEach((key) => {
+        addVariant(`pair-${key}-${variantName}`, ({ modifySelectors, separator }) => {
           modifySelectors(({ className }) => {
             return (
-              `.flag-${key}:${variant} ~ .${e(`flag-${key}-${variant}${separator}${className}`)},\n` +
-              `.flag-${key}:${variant} ~ * .${e(`flag-${key}-${variant}${separator}${className}`)}`
+              `.pair-${key}${state} ~ .pair-${key}-${variantName}\\${separator}${className},\n` +
+              `.pair-${key}${state} ~ * .pair-${key}-${variantName}\\${separator}${className}`
             )
           })
         })
       })
-    })
+    }
   },
   {
     theme: {
-      flag: {
-        1: '1',
-        2: '2',
-        3: '3',
+      pair: {
+        1: 1,
+        2: 2,
+        3: 3,
       },
-    }
+    },
   }
 )
 
